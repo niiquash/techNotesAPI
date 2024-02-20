2e725aa367a0311841a0b7ceb7100d20f21d7b1f
niiquash <qua16006@byui.edu>
2024-02-19 17:52:39 -0700

    database config

---
+++ models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  roles: [{ type: String, default: "Employee" }],
+  active: { type: Boolean, default: true },
+});
+
+module.exports = mongoose.model("User", userSchema);
